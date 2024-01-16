296d45925b6f34042683ebdde59e893b2a2450be
ClaireChilders <clairechilders@oakland.edu>
2024-01-16 17:03:27 -0500

    feat: implement parse_message
    NOTE: currently only works if there are no nested variables. Also will break if there are curly braces that aren't closed properly.

--- src/messager.py
+++ src/messager.py
@@ -21,13 +21,32 @@ class Messager:
         self.config.webdriver.get(url)
 
     def parse_message(self, student: Student, message: str):
-        pass
-        parsed_message = nestedExpr('{', '}').parseString(message).asList()
-        if len(parsed_message) == 0:
+        escaped_message = message.replace("\\{", "")
+        escaped_message = escaped_message.replace("\\}", "")
+        split_variables = escaped_message.split('{')
+
+        if len(split_variables) == 1:  # There are no variables
             return message
 
-        # TODO: Iterate through parsed_message list and replace variables with student data
-        return ''.join(parsed_message)
+        variable_message = ""
+
+        for i in range(0, len(split_variables)):
+            if "}" in split_variables[i]:
+                split_variable = split_variables[i].split("}")
+                student_data = student.get_student_data(split_variable[0])
+                if student_data is not None:
+                    logging.debug(
+                        f"Replacing [{split_variable[0]}] with {student_data}")
+                    variable_message += student_data
+                    variable_message += split_variable[1]
+                else:
+                    logging.warn(f"Student {student.student_id} does not have "
+                                 f"variable {split_variable[0]}")
+                    variable_message += split_variable[1]
+            else:
+                variable_message += split_variables[i]
+
+        return variable_message
 
     def open_handshake(self):
         pass
