f8702caed0e9fd06394b98f20955f3b9396cb7c3
ClaireChilders <clairechilders@oakland.edu>
2024-01-16 17:11:27 -0500

    feat: create logs directory

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from datetime import datetime as dt
 import logging
+import os
 
 
 logfile = f"logs/{dt.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+if not os.path.exists("logs"):
+    os.makedirs("logs")
 logging.basicConfig(
     filename=logfile,
     encoding='utf-8',
